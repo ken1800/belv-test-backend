6d8da989b32ac4093c74f3e8ec8eb3174adb1bea
ken1800 <kennethirungu1800@gmail.com>
2021-09-22 18:54:15 +0300

    set cors

--- middleware/mongodb.js
+++ middleware/mongodb.js
@@ -1,6 +1,25 @@
 import mongoose from 'mongoose';
 
+import Cors from 'cors';
+
+const cors = Cors({
+  methods: ['GET', 'HEAD', 'POST'],
+});
+
+const runMiddleware = (req, res, fn) => {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+};
+
 const connectDB = (handler) => async (req, res) => {
+  await runMiddleware(req, res, cors);
+
   if (mongoose.connections[0].readyState) {
     return handler(req, res);
   }
--- pages/api/getProducts.js
+++ pages/api/getProducts.js
@@ -4,7 +4,7 @@ import Products from '../../models/products';
 const handler = async (req, res) => {
   if (req.method === 'GET') {
     try {
-      const products = await Products.find({}).populate('category');
+      const products = await Products.find({}).populate('category').exec();
       return res.status(200).send(products);
     } catch (error) {
       return res.status(500).send(error.message);
