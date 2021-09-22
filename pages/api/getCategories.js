89ddfaae30424affe7ccc604d116f2930f8d9fc6
ken1800 <kennethirungu1800@gmail.com>
2021-09-22 19:23:14 +0300

    fixed mongoose populate

--- pages/api/getCategories.js
+++ pages/api/getCategories.js
@@ -1,10 +1,14 @@
 import connectDB from '../../middleware/mongodb';
 import Category from '../../models/productCategory';
+import Product from '../../models/products';
 
 const handler = async (req, res) => {
   if (req.method === 'GET') {
     try {
-      const categories = await Category.find({}).populate('products');
+      const categories = await Category.find({}).populate({
+        path: 'products',
+        model: Product,
+      });
       return res.status(200).send(categories);
     } catch (error) {
       return res.status(500).send(error.message);
--- pages/api/getProducts.js
+++ pages/api/getProducts.js
@@ -1,10 +1,16 @@
 import connectDB from '../../middleware/mongodb';
+import CategoryModel from '../../models/productCategory';
 import Products from '../../models/products';
 
 const handler = async (req, res) => {
   if (req.method === 'GET') {
     try {
-      const products = await Products.find({}).populate('category').exec();
+      const products = await Products.find({})
+        .populate({
+          path: 'category',
+          model: CategoryModel,
+        })
+        .exec();
       return res.status(200).send(products);
     } catch (error) {
       return res.status(500).send(error.message);
